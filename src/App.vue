d5f82abe667669af55974ffe7e4105937bb5f85b
Teo <cheesen.xu@gmail.com>
2026-01-16 12:09:42 +0800

    style: add pointer-events-none to background decoration elements in App.vue for improved interaction handling

--- src/App.vue
+++ src/App.vue
@@ -135,8 +135,8 @@ watchEffect(() => {
     @click.capture="handleGlobalClick"
   >
     <!-- Background Decoration -->
-    <div class="absolute top-1/4 left-1/4 w-96 h-96 bg-blue-900/10 rounded-full blur-3xl" />
-    <div class="absolute bottom-1/4 right-1/4 w-96 h-96 bg-purple-900/10 rounded-full blur-3xl" />
+    <div class="absolute top-1/4 left-1/4 w-96 h-96 bg-blue-900/10 rounded-full blur-3xl pointer-events-none" />
+    <div class="absolute bottom-1/4 right-1/4 w-96 h-96 bg-purple-900/10 rounded-full blur-3xl pointer-events-none" />
 
     <div
       class="main-slider flex h-full transition-transform duration-700 cubic-bezier"
